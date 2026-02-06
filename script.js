49bcfdb9ccc1f4bf35b0c3755ee313796f8bd10d
Chemeka D Pierce <cdpierce81@gmail.com>
2026-02-06 16:46:50 -0500

    Update product card HTML structure and classes

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ fetch('products.json')
         <div class="product-card">
           <img src="${product.image}" alt="${product.name}">
           <h3>${product.name}</h3>
-          <p>${product.price}</p>
-          <button>Add to Cart</button>
+          <p class = "price">${product.price}</p>
+          <button class = "add-btn">Add to Cart</button>
         </div>
       `;
     });
